bd94007e3e5f5f8564f212c0b60f4e9cb440a9f3
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-20 15:52:21 +0200

    creating file for concepts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ExpenseItems from './components/ExpenseItems';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <ExpenseItems/>
     </div>
   );
 }
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItems = () => {
+  return (
+    <div>
+      <h1 className='text-red-500'> Expenses items </h1>
+    </div>
+  )
+}
+
+export default ExpenseItems
